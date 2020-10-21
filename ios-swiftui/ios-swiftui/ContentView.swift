162d4a13a14932df9474828b51013758c5d8312a
Richard Robinson <kincade71@gmail.com>
2020-10-21 17:45:22 -0400

    Update ContentView.swift

--- ios-swiftui/ios-swiftui/ContentView.swift
+++ ios-swiftui/ios-swiftui/ContentView.swift
@@ -8,20 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    var sandwiches: [Sandwich] = []
     var body: some View {
-        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/){ item in
-            Image(systemName: "photo")
-            
-            VStack{
-                Text("bob")
-                Text("bill")
-            }
+// Begin Body
+        NavigationView{
+            List(sandwiches){ sandwich in
+                
+                Image(sandwich.imageName).resizable().aspectRatio(contentMode: .fit).cornerRadius(10)
+                
+                VStack(alignment:.leading){
+                    Text(sandwich.name).font(.largeTitle).multilineTextAlignment(.leading)
+                }
+                NavigationLink("", destination: SandwichDetail(sandwich: sandwich))
+            }.navigationTitle("Sandwiches")
         }
+        
+//        end body
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(sandwiches: dummyData)
     }
 }

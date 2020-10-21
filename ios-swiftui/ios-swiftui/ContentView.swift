95765c3c35535f0e9bd435c980609b3b2ab5f767
Richard Robinson <kincade71@gmail.com>
2020-10-21 15:06:38 -0400

    Update ContentView.swift

--- ios-swiftui/ios-swiftui/ContentView.swift
+++ ios-swiftui/ios-swiftui/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/){ item in
+            Image(systemName: "photo")
+            
+            VStack{
+                Text("bob")
+                Text("bill")
+            }
+        }
     }
 }
 
